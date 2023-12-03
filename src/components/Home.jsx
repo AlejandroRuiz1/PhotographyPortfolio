c4ff1ccde2e31bc1f02ba545235e1b24d221815b
Alejandro Ruiz <alejandro.ruiz2@upr.edu>
2023-12-03 16:16:12 -0600

    Implemented homepage skeleton with working react-router links for navigation. Using scss for styling.

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import '../styles/Home.scss';
+
+function Home() {
+  return (
+    <div>
+      <h1>HomePage</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import "../styles/Nav.scss";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <div className="Name">
+        <h1>Photographer Name</h1>
+      </div>
+      <div className="Links">
+        <Link className="link" to="/home">
+          <p>Home</p>
+        </Link>
+        <Link className="link" to="/about">
+          <p>About</p>
+        </Link>
+        <Link className="link" to="/contact">
+          <p>Contact Me</p>
+        </Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Navigation
